48854cf3a9a2cf14086ce4d17293c576c005eb3a
nltkhanh3009 <nltkhanh3009@gmial.com>
2025-03-28 13:56:48 +0700

    update-applicatim

---
+++ Test/Data/ApplicationDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test.Data
+{
+    public class ApplicationDb : DbContext
+    {
+        public ApplicationDb( DbContextOptions <ApplicationDb> options) :base(options)
+        {
+                
+        }
+    }
+}
